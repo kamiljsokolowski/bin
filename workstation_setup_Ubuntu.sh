b34d012b36b5a5c7d4ef53e607414c901a819e59
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2015-08-31 14:05:19 +0200

    office web services require Silverlight plugin

--- workstation_setup_Ubuntu.sh
+++ workstation_setup_Ubuntu.sh
@@ -14,7 +14,7 @@ sed -i -e '/extras.ubuntu.com/ s/# //' /etc/apt/sources.list            # Extras
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -; echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list           # Google Chrome
 sudo sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian '$(lsb_release -cs)' contrib non-free' > /etc/apt/sources.list.d/virtualbox.list"; wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -         # VirtualBox
 echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list; apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9         # Docker
-#apt-add-repository -y ppa:pipelight/stable
+apt-add-repository -y ppa:pipelight/stable          # open source Silverlight counterpart
 apt-get update
 
 
