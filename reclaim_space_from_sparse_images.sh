5cd8f0cca6fb34479064e8e7c23a215e197a9f43
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2015-09-14 14:00:08 +0200

    pass guest, image and guest user as script arguments

--- reclaim_space_from_sparse_images.sh
+++ reclaim_space_from_sparse_images.sh
@@ -4,33 +4,33 @@ set -o verbose
 set -o errexit
 #set -o nounset
 
-user='whoami'
-#guest=$1
-#guest_user=$2
-guest=
-guest_user=
-image=
+user=`whoami`
+guest=$1
+image=$2
+guest_user=$3
 
 ### add check if variables are present ###
 
-#if [ "$guest_user" = "" ]; then
-#	echo "User name (hit enter to use '$user')?"
-#	read guest_user
-#fi
-#
-#if [ "$guest_user" = "" ]; then
-#	guest_user=$user
-#fi
+if [ "$guest_user" = "" ]; then
+	echo "User name (hit enter to use '$user')?"
+	read guest_user
+fi
 
-# (on GUEST)
+if [ "$guest_user" = "" ]; then
+	guest_user=$user
+fi
+
+### (on GUEST) ###
 ssh -T $guest_user@$guest '
 sudo dd if=/dev/zero of=/mytempfile
 sudo rm -rf /mytempfile
 sudo shutdown -h now
 '
 
-# (on HOST)
+### (on HOST) ###
 sudo bash <<EOF
 mv /vmdata/storage/${image}.img /vmdata/storage/${image}.img.backup
 qemu-img convert -O qcow2 /vmdata/storage/${image}.img.backup /vmdata/storage/${image}.img
+virsh start --domain $image
 EOF
+
