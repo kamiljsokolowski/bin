c4e61d8dcba2eb8b4f13fa424a5019b39723b4b9
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2015-10-19 15:33:08 +0200

    add pip to the list of apps (required for Vim Powerline plugin setup)

--- workstation_setup_Ubuntu.sh
+++ workstation_setup_Ubuntu.sh
@@ -23,7 +23,7 @@ sudo apt-get update
 
 
 ### APPS ###
-sudo apt-get -y install curl tmux git vim irssi zsh
+sudo apt-get -y install curl tmux git vim irssi zsh python-pip
 # (optional) zsh + oh-my-zsh plugin (requires reboot)
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - |sudo zsh
 chsh -s `which zsh`
