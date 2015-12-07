27b59266ca32e0cb772264ad0f75a836d77fc8c4
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2015-12-07 22:16:45 +0100

    introduce system provisioning layers

--- workstation_Ubuntu.sh
+++ workstation_Ubuntu.sh
@@ -2,27 +2,28 @@
 set -x
 set -v
 
+### hardware
+# ...
+
+### core/base
 # enable additional repositories & add 3rd party ones
 sudo sed -i -e '/ partner/ s/# //' /etc/apt/sources.list         # Partner
 sudo sed -i -e '/extras.ubuntu.com/ s/# //' /etc/apt/sources.list            # Extras
 sudo sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian '$(lsb_release -cs)' contrib non-free' > /etc/apt/sources.list.d/virtualbox.list" && wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -           # Virtualbox
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - && echo "deb http://dl.google.com/linux/chrome/deb/ stable main" |sudo tee --append /etc/apt/sources.list.d/google.list         # Google Chrome repo
 sudo apt-add-repository -y ppa:pipelight/stable         # Silverlight OSS counterpart
+# update /etc/passwd hashing algorithm
+#sudo authconfig --passalgo=sha512 --update
+# allow root only on tty1
+#sudo echo "tty1" /etc/securetty
+# only root can view /root dir
+#sudo chmod 0700 /root
 
-
-# install APPS
+### sysadmin
 sudo apt-get update -q && apt-get install -y \
-    curl \
-    tmux \
-    git \
-    vim \
-    irssi \
-    zsh \
-    python-setuptools \
- && easy_install pip
-# (optional) zsh + oh-my-zsh plugin (requires reboot)
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - |sudo zsh
-chsh -s `which zsh`
+    htop
+
+### server
 # (headless) Virtualbox
 sudo apt-get -y install build-essential dkms VirtualBox-5.0
 cd /tmp && wget http://download.virtualbox.org/virtualbox/5.0.2/Oracle_VM_VirtualBox_Extension_Pack-5.0.2-102096.vbox-extpack
@@ -43,8 +44,21 @@ wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.4_x86_64.deb
 sudo dpkg -i vagrant_1.7.4_x86_64.deb
 sudo apt-get -y install libxslt-dev libxml2-dev libvirt-dev zlib1g-dev
 
+### apps
+sudo apt-get update -q && apt-get install -y \
+    curl \
+    tmux \
+    git \
+    vim \
+    irssi \
+    zsh \
+    python-setuptools \
+ && easy_install pip
+# (optional) zsh + oh-my-zsh plugin (requires reboot)
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - |sudo zsh
+chsh -s `which zsh`
 
-### USER ###
+### local
 # dotfiles
 git clone https://github.com/sokolowskik/dotfiles.git .dotfiles
 cd .dotfiles
