65b145da0375e82583cdec96d0d0a887100c8941
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2015-12-10 13:44:21 +0100

    update apps list with Dropbox cloud storage

--- workstation_Ubuntu.sh
+++ workstation_Ubuntu.sh
@@ -10,6 +10,7 @@ set -v
 sudo sed -i -e '/ partner/ s/# //' /etc/apt/sources.list         # Partner
 sudo sed -i -e '/extras.ubuntu.com/ s/# //' /etc/apt/sources.list            # Extras
 sudo sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian '$(lsb_release -cs)' contrib non-free' > /etc/apt/sources.list.d/virtualbox.list" && wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -           # Virtualbox
+echo "deb http://linux.dropbox.com/ubuntu $(lsb_release -cs) main" |sudo tee -a /etc/apt/sources.list.d/dropbox.list && sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E            # Dropbox
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - && echo "deb http://dl.google.com/linux/chrome/deb/ stable main" |sudo tee --append /etc/apt/sources.list.d/google.list         # Google Chrome repo
 sudo apt-add-repository -y ppa:pipelight/stable         # Silverlight OSS counterpart
 # update /etc/passwd hashing algorithm
@@ -54,6 +55,7 @@ sudo apt-get -y install libxslt-dev libxml2-dev libvirt-dev zlib1g-dev
 ### apps
 sudo apt-get update -q && sudo apt-get install -y \
     curl \
+    dropbox \
     tmux \
     git \
     vim \
