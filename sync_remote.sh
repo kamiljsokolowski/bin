6a3ee4f6bf0f5322558aa9b64377bb7d08d2ba3a
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2015-12-23 12:36:38 +0100

    check if directories are in sync

--- sync_remote.sh
+++ sync_remote.sh
@@ -4,29 +4,50 @@
 #set -o errexit
 #set -o nounset
 
+E_NOFILE=66
 E_NOARGS=75
 E_BADDIR=85
 
-current=`pwd`
-local=$1
-remote=$2
+CURRENT=`pwd`
+LOCAL=$1
+REMOTE=$2
 
+dir_exists ()
+{
+    dir=$1
+    if [ ! -d "${dir}" ]
+    then
+        echo "'${dir}' is not a directory"
+        return $E_BADDIR
+    fi
+}
+
+if_in_sync ()
+{
+    src=$1
+    targ=$2
+    for LINE in `find ${src}/ -type f -printf "%P\n"`
+    do
+        if [[ ! -f ${targ}/${LINE} ]]
+        then
+            echo "${LINE} was not synced"
+            exit $E_NOFILE
+        fi
+    done
+}
+
+### tests
 if [ $# -lt 2 ]; then
     echo "Usage: `basename $0 $1` [local] [remote]"
     exit $E_NOARGS
 fi
 
-check_dir ()
-{
-    if [ ! -d "${local}" ]
-    then
-        echo "'${local}' is not a directory"
-        return $E_BADDIR
-    fi
-}
+dir_exists ${LOCAL}
+dir_exists ${REMOTE}
 
-check_dir ${local}
-check_dir ${remote}
+### sync
+rsync -avz ${LOCAL}/ ${REMOTE}
 
-rsync -avz ${local} ${remote}
+### check if in sync
+if_in_sync ${LOCAL} ${REMOTE}
 
