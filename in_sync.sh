5af475634d2112d6b646b4873aa127a87c6f9058
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2015-12-26 09:52:55 +0100

    sync pre-defined locations (sysconfig)

--- in_sync.sh
+++ in_sync.sh
@@ -8,17 +8,17 @@ E_NOFILE=66
 E_NOARGS=75
 E_BADDIR=85
 
-CURRENT=`pwd`
-LOCAL=$1
-REMOTE=$2
+#CURRENT=`pwd`
+REMOTE=$1
 
 dir_exists ()
 {
     dir=$1
     if [ ! -d "${dir}" ]
     then
-        echo "'${dir}' is not a directory"
-        return $E_BADDIR
+        echo "'${dir}' does not exist. Creating..."
+        mkdir -p ${REMOTE}
+        echo "'${dir}' has been created."
     fi
 }
 
@@ -43,17 +43,23 @@ if_in_sync ()
 }
 
 ### tests
-if [ $# -lt 2 ]; then
-    echo "Usage: `basename $0 $1` [local] [remote]"
+if [ $# -lt 1 ]; then
+    echo "Usage: `basename $0` [remote]"
     exit $E_NOARGS
 fi
 
-dir_exists ${LOCAL}
 dir_exists ${REMOTE}
 
 ### sync
-rsync -aAXH --numeric-ids --info=progress2 ${LOCAL} ${REMOTE}
+# first create directory structure
+echo "Creating snapshot directory structure..."
+mkdir -p ${REMOTE}/snapshot-$(date +%d.%m.%y)/{etc} && echo "Snapshot directory structure has been created."
+# sysconfig
+echo "Synchronizing system configuration..."
+sudo rsync -aAXH --numeric-ids --info=progress2 /etc/ ${REMOTE}/snapshot-$(date +%d.%m.%y)/etc/
+echo "System configuration has been synchronized."
 
 ### check if in sync
-if_in_sync ${LOCAL} ${REMOTE}
+echo "Checking if system configuration BACKUP is in sync with the SOURCE directory..."
+if_in_sync /etc/ ${REMOTE}/snapshot-$(date +%d.%m.%y)/etc/
 
