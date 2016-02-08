40eb7a134a1bcb4510043078c2ff19fefb69dd16
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2016-02-08 15:23:53 +0100

    fix a minor permissions bug

--- keep_workstation_up_to_date.sh
+++ keep_workstation_up_to_date.sh
@@ -1,14 +1,14 @@
-#!/usr/local/env bash
+#!/usr/bin/env bash
 
 # db sync first
 sudo apt-get -q update
 
 # update the system
 sudo apt-get -y upgrade \
-&& apt-get -y dist-upgrade \
-&& apt-get -y autoremove \
-&& apt-get clean
+&& sudo apt-get -y dist-upgrade \
+&& sudo apt-get -y autoremove \
+&& sudo apt-get clean
 
 # update apps not managed via repos
-pip install --upgrade pip
+sudo pip install --upgrade pip
 
