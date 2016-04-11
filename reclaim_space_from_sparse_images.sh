b9b4d86fc40fad2d3f70d3d8f9fe14c089a99b97
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2016-04-11 11:54:35 +0200

    Backup the original image before filling it up with zeros

--- reclaim_space_from_sparse_images.sh
+++ reclaim_space_from_sparse_images.sh
@@ -20,17 +20,26 @@ if [ "$guest_user" = "" ]; then
 	guest_user=$user
 fi
 
-### (on GUEST) ###
+# host
+sudo bash <<EOF
+virsh destroy --domain ${image}
+cp /vmdata/storage/${image}.img /vmdata/storage/${image}.img.backup
+virsh start --domain ${image}
+sleep 60
+EOF
+
+# guest
 ssh -T $guest_user@$guest<<EOSSH
 sudo dd if=/dev/zero of=/mytempfile
 sudo rm -rf /mytempfile
-sudo shutdown -h now
 EOSSH
 
-### (on HOST) ###
+# (again) host
 sudo bash <<EOF
-mv /vmdata/storage/${image}.img /vmdata/storage/${image}.img.backup
-qemu-img convert -O qcow2 /vmdata/storage/${image}.img.backup /vmdata/storage/${image}.img
-virsh start --domain $image && rm -rf /vmdata/storage/${image}.img.backup
+virsh destroy --domain ${image}
+sleep 60
+mv /vmdata/storage/${image}.img /vmdata/storage/${image}.img.tmp
+qemu-img convert -O qcow2 /vmdata/storage/${image}.img.tmp /vmdata/storage/${image}.img
+virsh start --domain $image && rm -rf /vmdata/storage/${image}.img.tmp
 EOF
 
