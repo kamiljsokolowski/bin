08b5066a3d807e17426c5ca1a7dd10b37676df87
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2017-11-22 10:29:52 +0100

    unzip Packer bin to directory in PATH

--- Ubuntu/post-install_develop.sh
+++ Ubuntu/post-install_develop.sh
@@ -66,4 +66,5 @@ vagrant plugin install \
 
 echo '### install Packer ###'
 cd /tmp && wget https://releases.hashicorp.com/packer/1.1.2/packer_1.1.2_linux_amd64.zip
+unzip packer_1.1.2_linux_amd64.zip -d /usr/local/bin/
 cd
